8cad0e427197bcf4e16607a082e0024551c4dc6d
isabelbeka <beca6119@gmail.com>
2024-06-07 16:46:23 -0300

    Classe automovel/

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("aula de git")
+class Carro{
+    constructor(tipo,capacidade){
+       this.tipo = tipo;
+       this.capacidade = capacidade
+   }
+}
+
+const BMW = new Carro("carro",5);
